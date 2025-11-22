934871c130b0f5d792ef19ca6c4d505cf0159d87
TERESA MARGARITA ZAVALA CANTU <81946124+Mayte-sys@users.noreply.github.com>
2025-11-22 15:20:10 -0600

    Practica11

---
+++ Practica11/Practica11/src/java/web/EnviaDatos.java
@@ -0,0 +1,94 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
+ */
+package web;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author marga
+ */
+public class EnviaDatos extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        String fname;
+        String lname;
+        String phone;
+        String email;
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            /* TODO output your page here. You may use following sample code. */
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Servlet EnviaDatos</title>");
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h1>Servlet EnviaDatos at " + request.getContextPath() + "</h1>");
+            fname = request.getParameter("fname");
+            lname = request.getParameter("lname");
+            phone = request.getParameter("phone");
+            email = request.getParameter("email");
+            out.println("<h1>Servlet EnviaDatos at " + request.getParameter("fname") + "</h1>");
+            out.println("<p>Servlet EnviaDatos at " + lname + "</p>"); 
+            out.println("<p>Servlet EnviaDatos at " + phone + "</p>"); 
+            out.println("<p>Servlet EnviaDatos at " + email + "</p>"); 
+            out.println("</body>");
+            out.println("</html>");
+        }
+    }   // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+}
